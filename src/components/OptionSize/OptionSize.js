4790c330e71df88d9ecf3a9e7603a1810faa79a1
Dominik Mrówka <dmrowka7@gmail.com>
2022-05-21 09:45:49 +0200

    Add OptionSize component with styles

---
+++ src/components/OptionSize/OptionSize.js
@@ -0,0 +1,22 @@
+import styles from './OptionSize.module.scss'
+import shortid from 'shortid';
+import PropTypes from 'prop-types';
+
+const OptionSize = (props) => {
+     return (
+          <div className={styles.sizes}>
+               <h3 className={styles.optionLabel}>Sizes</h3>
+               <ul className={styles.choices}>
+                    {props.sizes.map(size => <li key={shortid()} ><button onClick={() => props.handleSetSizeAndPrice(size)} type="button" className={size.name === props.currentSize ? styles.active : undefined}> {size.name}</button></li>)}
+               </ul>
+          </div>
+     );
+};
+
+OptionSize.propType = {
+     sizes: PropTypes.string.isRequired,
+     handleSetSizeAndPrice: PropTypes.func.isRequired,
+     currentSize: PropTypes.string.isRequired,
+}
+
+export default OptionSize
