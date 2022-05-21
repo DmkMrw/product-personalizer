865b2a0bbd5abededaf3ded6eefcc717cee461fc
Dominik Mrówka <dmrowka7@gmail.com>
2022-05-21 10:28:06 +0200

    Add OptionColor component with styles

---
+++ src/components/OptionColor/OptionColor.js
@@ -0,0 +1,23 @@
+import styles from './OptionColor.module.scss'
+import shortid from "shortid";
+import clsx from "clsx";
+import PropTypes from 'prop-types'
+
+
+
+const OptionColor = (props) => {
+     return (
+          <div className={styles.colors}>
+               <h3 className={styles.optionLabel}>Colors</h3>
+               <ul className={styles.choices}>
+                    {props.colors.map(item => <li key={shortid()}><button onClick={() => props.handleSetColor(item)} type="button" className={clsx(props.prepareColorClassName(item), item === props.currentColor && styles.active)} /></li>)}
+               </ul>
+          </div>
+     );
+}
+
+OptionColor.propTypes = {
+
+}
+
+export default OptionColor;
